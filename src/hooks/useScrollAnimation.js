e92cfb33f18783d894a128ba6988a720b7a4ad6b
AldG99 <a.garciaglz17@gmail.com>
2025-02-08 23:40:24 -0600

    Agregar carpeta hooks para la gestión de lógica reutilizable en el proyecto

---
+++ src/hooks/useScrollAnimation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UseScrollAnimation = () => {
+  return <div>UseScrollAnimation</div>;
+};
+
+export default UseScrollAnimation;
+
+// Para animaciones basadas en scroll.
---
+++ src/hooks/useTheme.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UseTheme = () => {
+  return <div>UseTheme</div>;
+};
+
+export default UseTheme;
+
+// Manejo del tema claro/oscuro.
