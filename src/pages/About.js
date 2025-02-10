b5730ac4f30d4334eeb8d9fdeb86e6fb96b3fdf5
AldG99 <a.garciaglz17@gmail.com>
2025-02-09 20:43:31 -0600

    Añadir carpeta pages para organizar las vistas principales de la aplicación

---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => {
+  return <div>About</div>;
+};
+
+export default About;
+
+// Página detallada sobre experiencia y trayectoria.
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return <div>Portfolio</div>;
+};
+
+export default Portfolio;
+
+// Galería de proyectos con filtros y animaciones.
