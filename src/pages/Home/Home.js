6f88814bdc900ba21e8d8e8de60f4813ca366453
Alfredo Garcia <95046266+AldG99@users.noreply.github.com>
2025-05-09 13:16:49 -0600

    Implementar Home con scroll al inicio y título de documento

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import HeroSection from '../../components/HeroSection/HeroSection';
+import FeaturedProjects from '../../components/FeaturedProjects/FeaturedProjects';
+import './Home.scss';
 
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    // Scroll to top when component mounts
+    window.scrollTo(0, 0);
+    // Set document title
+    document.title = 'Portafolio | Inicio';
+  }, []);
+
+  return (
+    <main className="home">
+      <HeroSection />
+      <FeaturedProjects />
+    </main>
+  );
 };
 
 export default Home;
