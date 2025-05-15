5ae0d39e3a252b805b4b048924d29c7f970f34c2
Alfredo Garcia <95046266+AldG99@users.noreply.github.com>
2025-05-15 15:29:51 -0600

    Crear ScrollRevealProvider para animaciones al hacer scroll

--- src/App.js
+++ src/App.js
@@ -6,23 +6,26 @@ import Home from './pages/Home/Home';
 import Projects from './pages/Projects/Projects';
 import About from './pages/About/About';
 import ProjectDetailSwitch from './components/ProjectDetail/ProjectDetailSwitch';
+import { ScrollRevealProvider } from './context/ScrollRevealContext';
 import './App.scss';
 
 const App = () => {
   return (
     <Router>
-      <div className="app">
-        <Header />
-        <div className="app__content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/trabajo" element={<Projects />} />
-            <Route path="/:projectSlug" element={<ProjectDetailSwitch />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
+      <ScrollRevealProvider>
+        <div className="app">
+          <Header />
+          <div className="app__content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/trabajo" element={<Projects />} />
+              <Route path="/:projectSlug" element={<ProjectDetailSwitch />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </ScrollRevealProvider>
     </Router>
   );
 };
